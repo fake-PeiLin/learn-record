cb5968497e4b6b69fb0802a9fec1fdaf07f39738
real-Pei-Lin <18398322316@163.com>
2019-09-03 16:49:44 +0800

    after 133

--- mxg-msm/src/router.js
+++ mxg-msm/src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 // import Login from './views/login/index.vue'
 import Login from './views/login'
+import Layout from '@/components/Layout.vue'
  
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path:'/login',
       name:'login',
       component:Login
+    },
+    {
+      path:'/',
+      name:'layout',
+      component:Layout
     }
   ]
 });
