4c5b14ea8575652085355c591f08b68f5d3c916d
real-Pei-Lin <18398322316@163.com>
2019-09-10 00:18:39 +0800

    after 142

--- mxg-msm/src/views/member/index.vue
+++ mxg-msm/src/views/member/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>会员管理</div>
+  <div>
+    <el-table :data="list" height="380" border style="width: 100%">
+      <el-table-column type="index" label="序号"></el-table-column>
+      <el-table-column prop="cardNum" label="会员卡号"></el-table-column>
+      <el-table-column prop="name" label="会员姓名"></el-table-column>
+      <el-table-column prop="birthday" label="会员生日"></el-table-column>
+      <el-table-column prop="phone" label="手机号码"></el-table-column>
+      <el-table-column prop="integral" label="可用积分"></el-table-column>
+      <el-table-column prop="money" label="开卡金额"></el-table-column>
+      <el-table-column prop="payType" label="支付类型"></el-table-column>
+      <el-table-column prop="address" label="会员地址"></el-table-column>
+      <el-table-column label="操作"  width="150">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.row.id)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -21,8 +39,14 @@ export default {
         const resp = response.data;
         this.list = resp.data;
         console.log(resp);
-      });
-    }
+      })
+    },
+    handleEdit(id){
+        console.log('编辑',id)
+    },
+    handleDelete(id){
+        console.log('删除',id)
+    },
   }
 };
 </script>
