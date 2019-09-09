eaf525da55801ba0c316cd4bd34a804593ac075a
real-Pei-Lin <18398322316@163.com>
2019-09-09 23:27:59 +0800

    after 141

--- mxg-msm/src/views/member/index.vue
+++ mxg-msm/src/views/member/index.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        会员管理
-    </div>
+  <div>会员管理</div>
 </template>
+
+<script>
+import memberApi from "@/api/member";
+export default {
+  data() {
+    return {
+      list: []
+    };
+  },
+
+  created() {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData() {
+      memberApi.getList().then(response => {
+        const resp = response.data;
+        this.list = resp.data;
+        console.log(resp);
+      });
+    }
+  }
+};
+</script>
