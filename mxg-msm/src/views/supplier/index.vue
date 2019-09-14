af7ace6f60ac201c88ef8f424d65d23a67a89a2f
real-Pei-Lin <18398322316@163.com>
2019-09-14 16:44:58 +0800

    after 158/ finished 13.4.1/ NMP

--- mxg-msm/src/views/supplier/index.vue
+++ mxg-msm/src/views/supplier/index.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <el-form ref="searchForm" :inline="true" :model="searchMap" style="margin-top:20px">
+      <el-form-item prop="name">
+        <el-input v-model="searchMap.name" placeholder="供应商名称" style="width:200px"></el-input>
+      </el-form-item>
+      <el-form-item prop="linkman">
+        <el-input v-model="searchMap.linkman" placeholder="联系人" style="width:200px"></el-input>
+      </el-form-item>
+      <el-form-item prop="mobile">
+        <el-input v-model="searchMap.mobile" placeholder="联系电话" style="width:200px"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="fetchData">查询</el-button>
+      </el-form-item>
+    </el-form>
     <el-table :data="list" height="380" border style="width: 100%">
       <el-table-column type="index" label="序号"></el-table-column>
       <el-table-column prop="name" label="供应商名称"></el-table-column>
@@ -34,7 +48,11 @@ export default {
       pageSize: 10,
       currentPage: 1,
       total: 0,
-      searchMap: {}
+      searchMap: {
+        name: "",
+        linkman: "",
+        mobile: ""
+      }
     };
   },
 
