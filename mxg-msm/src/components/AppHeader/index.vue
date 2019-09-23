a4ecbb3c7bfff66e730bf3edd5d450294b820a05
real-Pei-Lin <18398322316@163.com>
2019-09-24 01:07:05 +0800

    after 179

--- mxg-msm/src/components/AppHeader/index.vue
+++ mxg-msm/src/components/AppHeader/index.vue
@@ -5,50 +5,91 @@
       <span class="company">梦学谷会员管理系统</span>
     </a>
 
-    <el-dropdown @command="handleCommand" >
+    <el-dropdown @command="handleCommand">
       <span class="el-dropdown-link">
         下拉菜单
         <i class="el-icon-arrow-down el-icon--right"></i>
       </span>
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item icon="el-icon-edit" command="a">修改密码</el-dropdown-item>
-        <el-dropdown-item icon="el-icon-s-fold" command="b" >退出系统</el-dropdown-item>
+        <el-dropdown-item icon="el-icon-s-fold" command="b">退出系统</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
+
+    <el-dialog title="修改密码" :visible.sync="dialogFormVisible" width="400px">
+      <el-form
+      :model="ruleForm"
+      status-icon
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      style="width: 300px"
+    >
+      <el-form-item label="原密码" prop="oldPass">
+        <el-input type="password" v-model="ruleForm.oldPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="新密码" prop="pass">
+        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+    </el-dialog>
+
+    
   </div>
 </template>
 
 <script>
-import {logout} from '@/api/login.js' 
+import { logout } from "@/api/login.js";
 export default {
-    methods:{
-        handleCommand(command){
-          switch(command){
-            case 'a':
-              this.$message(`点击修改密码`)
-              break;
-            case 'b':
-              logout( localStorage.getItem('mxg-msm-token')).then(response=>{
-                const resp = response.data
-                if(resp.flag){
-                  localStorage.removeItem('mxg-msm-token')
-                  localStorage.removeItem('mxg-msm-user')
-                  this.$router.push('/login')
-                }else{
-                  this.$message({
-                    message:resp.message,
-                    type:'warning',
-                    duration:500
-                  });
-                }
-              })
-              break;
-            default:
-              break;
-          }
-        }
+  data(){
+    return{
+      dialogFormVisible: false,
+      ruleForm: {},
+      rules:{}
     }
-}
+  },
+
+  methods: {
+    handleCommand(command) {
+      switch (command) {
+        case "a":
+          this.handlePwd();
+          break;
+        case "b":
+          this.handleLogout();
+          break;
+        default:
+          break;
+      }
+    },
+    handleLogout() {
+      logout(localStorage.getItem("mxg-msm-token")).then(response => {
+        const resp = response.data;
+        if (resp.flag) {
+          localStorage.removeItem("mxg-msm-token");
+          localStorage.removeItem("mxg-msm-user");
+          this.$router.push("/login");
+        } else {
+          this.$message({
+            message: resp.message,
+            type: "warning",
+            duration: 500
+          });
+        }
+      });
+    },
+    handlePwd(){
+      this.dialogFormVisible=true
+    }
+  }
+};
 </script>
 
 <style scoped>
@@ -60,12 +101,12 @@ export default {
   position: absolute;
   color: white;
 }
-.el-dropdown{
-    float:right;
-    margin-right:40px;
+.el-dropdown {
+  float: right;
+  margin-right: 40px;
 }
-.el-dropdown-link{
-    color:white;
-    cursor:pointer;
+.el-dropdown-link {
+  color: white;
+  cursor: pointer;
 }
 </style>
