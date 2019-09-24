56454542a36fc62b69620c80b0543eded45d2647
real-Pei-Lin <18398322316@163.com>
2019-09-24 16:32:19 +0800

    after 182

--- mxg-msm/src/utils/request.js
+++ mxg-msm/src/utils/request.js
@@ -1,25 +1,47 @@
 import axios from 'axios'
+import { Loading } from 'element-ui'
 
-// axios.get('/db.json').then(response => {
-//     const data = response.data
-//     console.log(data)
-// })
+const loading = {
+    loadingInstance: null,
+    open: function () {
+        if (this.loadingInstance === null) {
+            console.log('创建加载实例。。。')
+            this.loadingInstance = Loading.service({
+                target: '.main',
+                text: '拼命加载中...',
+                background: 'rgba(0,0,0,0.5)'
+            })
+        }
+
+    },
+
+    close: function () {
+        if (this.loadingInstance !== null) {
+            this.loadingInstance.close()
+        }
+        this.loadingInstance=null
+    }
+}
 
 const request = axios.create({
     baseURL: process.env.VUE_APP_BASE_API,
     // baseURL:'/',
-    timeout:5000
+    timeout: 5000
 })
 
-request.interceptors.request.use(config=>{
+request.interceptors.request.use(config => {
+    loading.open()
     return config
-},error=>{
+}, error => {
+    loading.close()
     return Promise.reject(error);
 })
 
-request.interceptors.response.use(response=>{
+request.interceptors.response.use(response => {
+    loading.close()
     return response
-},error=>{
+}, error => {
+    loading.close()
     return Promise.reject(error);
 })
 
