c2a0e0c75d31c49f67fb4d01630506754fa2c476
real-Pei-Lin <18398322316@163.com>
2019-10-07 11:55:39 +0800

    122/6.1

---
+++ mxg-diy/src/utils/request.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const request = axios.create({
+    baseURL:'/',
+    timeout:5000
+})
+
+// 请求拦截器
+request.interceptors.request.use(config => {
+    // 请求拦截
+    return config
+}, error => {
+    // 出现异常
+    return Promise.reject(error);
+})
+// 响应拦截器
+request.interceptors.response.use(response =>{
+    // response.data
+    return response
+}, error => {
+    return Promise.reject(error);
+})
+
+//  http://localhost:8888/dev-api/db.json 404
+// request.get('/db.json').then(response => {
+//     console.log(response.data)
+// })
+
+export default request // 导出自定义创建 axios 对象
