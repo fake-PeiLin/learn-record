c38c33d62788c9a29b9cdc0006dadd7c262bc222
real-Pei-Lin <18398322316@163.com>
2019-10-08 01:23:19 +0800

    128/7.4表单校验

--- mxg-diy/src/views/login/index.vue
+++ mxg-diy/src/views/login/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="login-container">
-    <el-form ref="form" :model="form" label-width="80px" class="login-form">
-        <h2 class="login-title">梦学谷会员管理系统</h2>
-      <el-form-item label="账号">
+    <el-form ref="form" :rules="rules" :model="form" label-width="80px" class="login-form">
+      <h2 class="login-title">梦学谷会员管理系统</h2>
+      <el-form-item label="账号" prop="username">
         <el-input v-model="form.username"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="密码" prop="password">
         <el-input v-model="form.password" type="password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">登录</el-button>
+        <el-button type="primary" @click="submitForm('form')">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -20,35 +20,48 @@ export default {
   data() {
     return {
       form: {
-          username:'',
-          password:''
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "账号不能为空", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "密码不能为空", trigger: "blur" }]
       }
     };
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    submitForm(formName) {
+        this.$refs[formName].validate(valid => {
+          if (valid) {
+              
+          } else {
+            console.log("验证失败");
+            return false;
+          }
+        });
     }
   }
 };
 </script>
 
 <style scoped>
-    .login-form{
-        width:350px;
-        margin:160px auto;
-        background-color: rgb(255,255,255,0.8);
-        padding: 28px;
-        border-radius: 20px;
-    }
-    .login-container{
-        position:absolute;
-        width:100%;
-        height:100%;
-        background:url('../../assets/login.jpg')
-    }
-    .login-title{
-        color:#303133;
-        text-align: center;
-    }
+.login-form {
+  width: 350px;
+  margin: 160px auto;
+  background-color: rgb(255, 255, 255, 0.8);
+  padding: 28px;
+  border-radius: 20px;
+}
+.login-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url("../../assets/login.jpg");
+}
+.login-title {
+  color: #303133;
+  text-align: center;
+}
 </style>
