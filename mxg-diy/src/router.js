5a01f1558ae1e2eb7be87b0a370f1fcf88b2202c
real-Pei-Lin <18398322316@163.com>
2019-10-08 16:52:51 +0800

    136/ 8.6所有菜单路由配置

--- mxg-diy/src/router.js
+++ mxg-diy/src/router.js
@@ -4,6 +4,10 @@ import Router from "vue-router";
 import Login from './views/login';
 import Layout from '@/components/Layout.vue'
 import Home from './views/home'
+import Member from './views/member'
+import Supplier from './views/supplier'
+import Goods from './views/goods'
+import Staff from './views/staff'
 
 Vue.use(Router);
 
@@ -24,6 +28,50 @@ export default new Router({
                     path: '/home',
                     component: Home,
                     meta: { title: '首页' }
+                }
+            ]
+        },
+        {
+            path:'/member',
+            component: Layout,
+            children:[
+                {
+                    path:'/',
+                    component:Member,
+                    meta:{title:'会员管理'}
+                }
+            ]
+        },
+        {
+            path:'/supplier',
+            component: Layout,
+            children:[
+                {
+                    path:'/',
+                    component:Supplier,
+                    meta:{title:'供应商管理'}
+                }
+            ]
+        },
+        {
+            path:'/goods',
+            component: Layout,
+            children:[
+                {
+                    path:'/',
+                    component:Goods,
+                    meta:{title:'商品管理'}
+                }
+            ]
+        },
+        {
+            path:'/staff',
+            component: Layout,
+            children:[
+                {
+                    path:'/',
+                    component:Staff,
+                    meta:{title:'员工管理'}
                 }
             ]
         },
