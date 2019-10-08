506093ee0a69ca98aab083ccbd2d83fd0043ea49
real-Pei-Lin <18398322316@163.com>
2019-10-08 16:11:40 +0800

    135/ 8.5左侧导航实现，视频与教程顺序有区别

--- mxg-diy/src/components/Layout.vue
+++ mxg-diy/src/components/Layout.vue
@@ -42,7 +42,7 @@ export default {
     bottom:0px;
     padding:10px;
     overflow-y:auto;
-    background-color:red;
+    /* background-color:red; */
 }
 </style>
 
--- mxg-diy/src/router.js
+++ mxg-diy/src/router.js
@@ -3,20 +3,29 @@ import Router from "vue-router";
 // import Login from './views/login/index.vue'
 import Login from './views/login';
 import Layout from '@/components/Layout.vue'
+import Home from './views/home'
 
 Vue.use(Router);
 
 export default new Router({
-    routes:[
+    routes: [
         {
-            path:'/login',
-            name:'login',
-            component:Login
+            path: '/login',
+            name: 'login',
+            component: Login
         },
         {
-            path:'/',
-            name:'layout',
-            component:Layout
-        }
+            path: '/',
+            name: 'layout',
+            component: Layout,
+            redirect:'/home',
+            children: [
+                {
+                    path: '/home',
+                    component: Home,
+                    meta: { title: '首页' }
+                }
+            ]
+        },
     ]
 });
