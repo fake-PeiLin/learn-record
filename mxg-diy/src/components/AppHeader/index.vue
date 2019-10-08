cbb638aee56ff8781500df0be4402440750ad715
real-Pei-Lin <18398322316@163.com>
2019-10-08 13:54:03 +0800

    133/ 8.4头部区域实现

--- mxg-diy/src/components/AppHeader/index.vue
+++ mxg-diy/src/components/AppHeader/index.vue
@@ -1,3 +1,48 @@
 <template>
-      <div class="header">头部1</div>
+  <div class="header">
+    <a href="#/">
+      <img class="logo" src="@/assets/logo.png" width="25px" />
+      <span class="company">梦学谷会员管理系统</span>
+    </a>
+
+    <el-dropdown @command="handleCommand">
+      <span class="el-dropdown-link">
+        下拉菜单
+        <i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item icon="el-icon-edit" command="a">修改密码</el-dropdown-item>
+        <el-dropdown-item icon="el-icon-s-fold" command="b">退出系统</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
 </template>
+
+<script>
+export default {
+    methods:{
+        handleCommand(command){
+            this.$message(`点击了${command}`)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .logo{
+        vertical-align: middle;
+        padding:0px 10px 0 40px;
+    }
+    .company{
+        position:absolute;
+        color:white;
+    }
+    .el-dropdown{
+        float:right;
+        margin-right:40px;
+    }
+    .el-dropdown-link{
+        color:white;
+        cursor:pointer;
+    }
+</style>
