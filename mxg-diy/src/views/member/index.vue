4a5181d93c5b4269d38ba343e6069a8dca2adc93
real-Pei-Lin <18398322316@163.com>
2019-10-09 16:46:07 +0800

    141/ 9.2.2 API调用接口

--- mxg-diy/src/views/member/index.vue
+++ mxg-diy/src/views/member/index.vue
@@ -3,3 +3,29 @@
     会员管理
   </div>
 </template>
+
+<script>
+import memberApi from '@/api/member'
+
+export default {
+    data(){
+      return{
+        list:[]
+      }
+    },
+
+    created() {
+      this.fetchData()
+    },
+
+    methods: {
+      fetchData(){
+        memberApi.getList().then(response=>{
+          const resp=response.data
+          this.list=resp.data
+          console.log(resp)
+        })
+      }
+    },
+}
+</script>
