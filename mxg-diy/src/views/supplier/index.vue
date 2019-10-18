2cb524b098dbb9b465b80a6d9da76a73b4b562ac
real-Pei-Lin <18398322316@163.com>
2019-10-19 01:20:39 +0800

    dont know clearly

--- mxg-diy/src/views/supplier/index.vue
+++ mxg-diy/src/views/supplier/index.vue
@@ -3,3 +3,29 @@
     供应商管理
   </div>
 </template>
+
+<script>
+import supplierApi from '@/api/supplier'
+
+export default {
+  data(){
+    return{
+      list:[]
+    }
+  },
+
+  created() {
+    this.fetchData()
+  },
+
+  methods: {
+    fetchData(){
+        console.log('1',this.list)
+      supplierApi.getlist().then(response=>{
+        this.list=response.data.data
+        console.log(this.list)
+      })
+    }
+  },
+}
+</script>
