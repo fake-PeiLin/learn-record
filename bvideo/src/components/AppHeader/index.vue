e8c61c2cca6a3852529fe418f551d3d1581bd478
real-Pei-Lin <18398322316@163.com>
2019-10-20 11:58:49 +0800

    8.9

--- bvideo/src/components/AppHeader/index.vue
+++ bvideo/src/components/AppHeader/index.vue
@@ -19,32 +19,50 @@
 </template>
 
 <script>
+import { logout } from "@/api/login";
 export default {
-    methods: {
-        handleCommand(command){
-            this.$message('click on item' + command);
-        }
-    },
+  methods: {
+    handleCommand(command) {
+      switch (command) {
+        case "a":
+          // 修改密码
+          break;
+        case "b":
+          // 退出系统
+          logout(
+            localStorage.getItem(localStorage.getItem("mgx-msm-token"))
+          ).then(response => {
+            // 清除本地数据
+            localStorage.removeItem("mgx-msm-token");
+            localStorage.removeItem("mgx-msm-user");
+            this.$router.push("/login");
+          });
+          break;
+        default:
+          break;
+      }
+    }
+  }
 };
 </script>
 
 <style scoped>
 .logo {
-vertical-align: middle;
-/* 上 右 下 左 */
-padding: 0 10px 0 40px;
+  vertical-align: middle;
+  /* 上 右 下 左 */
+  padding: 0 10px 0 40px;
 }
 .company {
-position: absolute;
-color: white;
-} 
+  position: absolute;
+  color: white;
+}
 /* 下拉菜单 */
 .el-dropdown {
-float: right;
-margin-right: 40px;
+  float: right;
+  margin-right: 40px;
 }
 .el-dropdown-link {
-cursor: pointer;
-color: #fff;
-} 
+  cursor: pointer;
+  color: #fff;
+}
 </style>
--- bvideo/src/views/home/index.vue
+++ bvideo/src/views/home/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>home</div>
+  <div>
+      <h1>欢迎访问Peilin会员管理系统</h1>
+  </div>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    div{
+        text-align: center;
+    }
+</style>
