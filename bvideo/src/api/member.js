e3c157447af53a4e6cbd09e235a673759b264b31
real-Pei-Lin <18398322316@163.com>
2019-10-20 12:24:11 +0800

    9.2.4

---
+++ bvideo/src/api/member.js
@@ -0,0 +1,10 @@
+import request from "@/utils/request"
+export default {
+    // 获取会员列表
+    getList() {
+        return request({
+            url: '/member/list',
+            method: 'get'
+        })
+    },
+}
